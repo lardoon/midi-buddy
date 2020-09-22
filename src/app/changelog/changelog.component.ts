d6b8800affd29a3de7c3eeb1a7838f4d3686867b
lardoon <petewzone-github@yahoo.com>
2020-09-22 23:39:33 +0100

    2 files modified

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -9,6 +9,20 @@ export class ChangelogComponent implements OnInit {
 
   constructor() { }
 
+  onePressSongCreatorlog: {[key:string]:string[]} = {
+    "1.0.2": [
+      "Added 3 user kits including Bosendorfer Jazz Trio 60+C1"
+    ],
+    "1.0.1": [
+      "Added Premium kits"
+    ],
+    "1.0.0": [
+      "Initial release",
+      "Included Default V2 kits in kit selector",
+      "Included 4 user kits in kit selector"
+    ]
+  }
+
   ngOnInit() {
   }
 
