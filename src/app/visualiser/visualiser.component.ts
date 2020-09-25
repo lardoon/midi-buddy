2d33ff73a092830e951ea46ea53192ef4c848393
lardoon <petewzone-github@yahoo.com>
2020-09-25 22:09:36 +0100

    2 files modified, 1 added

--- src/app/visualiser/visualiser.component.ts
+++ src/app/visualiser/visualiser.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Midi } from '@tonejs/midi';
+
+import * as mm from '@magenta/music/es6';
 
 @Component({
   selector: 'app-visualiser',
@@ -6,6 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualiser.component.css']
 })
 export class VisualiserComponent implements OnInit {
+
+  @ViewChild('visualiserCanvas')
+  visualiserCanvas: HTMLCanvasElement;
+
+  visualiser: mm.Visualizer;
+
+  private _midi: Midi;
+
+  get midi() : Midi {
+    return this._midi;
+  }
+
+  set midi(value: Midi) {
+    this._midi = value;
+    if(value) {
+      this.visualise();
+    }
+  }
+
+  visualise() {
+    const sequence = mm.midiToSequenceProto(this.midi.toArray());
+    this.visualiser =  new mm.Visualizer(sequence, this.visualiserCanvas);
+  }
 
   constructor() { }
 
