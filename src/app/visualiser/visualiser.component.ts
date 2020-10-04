38257f84e65779b0728fcba6e3c780fefb3d3614
lardoon <petewzone-github@yahoo.com>
2020-10-04 12:55:04 +0100

    3 files modified

--- src/app/visualiser/visualiser.component.ts
+++ src/app/visualiser/visualiser.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef,  Input, OnInit, ViewChild } from '@angular/core'
 import { Midi } from '@tonejs/midi';
 
 import * as mm from '@magenta/music/es6';
+import * as vm from 'html-midi-visualizer';
 
 @Component({
   selector: 'app-visualiser',
