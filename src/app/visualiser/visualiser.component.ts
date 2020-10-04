f95537876d3f1727bc817c5eb30746ac51660721
lardoon <petewzone-github@yahoo.com>
2020-10-04 19:57:45 +0100

    2 files modified

--- src/app/visualiser/visualiser.component.ts
+++ src/app/visualiser/visualiser.component.ts
@@ -10,12 +10,45 @@ import * as mm from '@magenta/music/es6';
 })
 export class VisualiserComponent implements OnInit, AfterViewInit {
 
-  noteSequence: mm.NoteSequence;
+  @ViewChild('svg')
+  svgElement: ElementRef<SVGSVGElement>;
 
   @Input()
   type: string = VisualiserType.SVG;
+  private _midi: Midi;
 
+  @Input()
+  set midi(value: Midi) {
+    if(value !== this._midi)
+      this._midi = value;
+    if(value)
+      this.visualise();
+  }
 
+  get midi() : Midi {
+    return this._midi;
+  }
+  _trackIndex: number;
+  @Input()
+  set trackIndex(value: number) {
+    if(this._trackIndex !== value) {
+      this._trackIndex = value;
+      this.visualise();
+    }
+  }
+  get trackIndex(): number {
+    return this._trackIndex;
+  };
+
+  visualise() {
+    let trackMidi = new Midi();
+    trackMidi.header = this.midi.header;
+    let newTrack = trackMidi.addTrack();
+    let track = this.midi.tracks[this.trackIndex];
+    newTrack.fromJSON(newTrack.toJSON());
+    let noteSequence = mm.midiToSequenceProto(trackMidi.toArray());
+    let visualiser = new mm.PianoRollSVGVisualizer(noteSequence, this.svgElement.nativeElement);
+  }
 
   constructor() { }
 
@@ -24,7 +57,7 @@ export class VisualiserComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-
+    
   }
 
 }
